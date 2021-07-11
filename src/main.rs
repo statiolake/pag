05983d72dc4d348bd2b5044b925f2b3e9c0899d5
statiolake <statiolake@gmail.com>
2021-07-12 02:01:19 +0900

    Remove termcolor dependency
    crossterm seems enough.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use crossterm::cursor::MoveTo;
 use crossterm::event::read;
 use crossterm::event::{Event, KeyCode};
+use crossterm::queue;
+use crossterm::style::{Color, Print, PrintStyledContent, Stylize};
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
-use crossterm::terminal::{Clear, ClearType};
-use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
+use crossterm::terminal::{Clear, ClearType, EnterAlternateScreen, LeaveAlternateScreen};
 use crossterm::QueueableCommand;
-use once_cell::sync::Lazy;
 use scopeguard::defer;
 use std::cell::Cell;
 use std::cell::RefCell;
@@ -13,14 +13,9 @@ use std::cmp::min;
 use std::env::args;
 use std::fs::read_to_string;
 use std::io::prelude::*;
-use std::io::stdin;
+use std::io::{stdin, stdout};
 use std::mem::take;
-use std::sync::Mutex;
-use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 use unicode_width::UnicodeWidthChar;
-
-static STDOUT: Lazy<Mutex<StandardStream>> =
-    Lazy::new(|| Mutex::new(StandardStream::stdout(ColorChoice::Auto)));
 
 pub enum MoveUnit {
     Line,
@@ -66,9 +61,9 @@ fn main() -> anyhow::Result<()> {
     }
 
     // enable alternate screen
-    STDOUT.lock().unwrap().queue(EnterAlternateScreen).unwrap();
+    stdout().queue(EnterAlternateScreen).unwrap();
     defer! {
-        STDOUT.lock().unwrap().queue(LeaveAlternateScreen).unwrap();
+        stdout().queue(LeaveAlternateScreen).unwrap();
     }
 
     let mut orig_query = None;
@@ -212,6 +207,7 @@ impl Screen {
     pub fn prev(&mut self) {
         if self.query.is_empty() {
             *self.message.borrow_mut() = Some("search query is not set".to_string());
+            self.needs_update.set(true);
             return;
         }
 
@@ -236,6 +232,7 @@ impl Screen {
     pub fn next(&mut self) {
         if self.query.is_empty() {
             *self.message.borrow_mut() = Some("search query is not set".to_string());
+            self.needs_update.set(true);
             return;
         }
 
@@ -261,34 +258,40 @@ impl Screen {
             return;
         }
 
-        let mut stdout = STDOUT.lock().unwrap();
-        stdout.queue(MoveTo(0, 0)).unwrap();
+        let stdout = stdout();
+        let mut stdout = stdout.lock();
+
         let start = self.current_top as usize;
         let end = min(self.lines.len(), start + self.contents_height());
         debug_assert!(end <= self.lines.len());
-        for line in &self.lines[start..end] {
-            let mut segments = vec![];
-            if self.query.is_empty() {
-                segments.push((ColorSpec::new(), &**line));
-            } else {
-                let mut curr_idx = 0;
-                for (next_idx, substr) in line.match_indices(&self.query) {
-                    let normal = &line[curr_idx..next_idx];
-                    let mut match_color = ColorSpec::new();
-                    match_color.set_fg(Some(Color::Red));
-                    segments.push((ColorSpec::new(), normal));
-                    segments.push((match_color, substr));
-                    curr_idx = next_idx + substr.len();
-                }
-                segments.push((ColorSpec::new(), &line[curr_idx..]));
-            };
-            stdout.queue(Clear(ClearType::CurrentLine)).unwrap();
-            for (spec, segment) in segments {
-                stdout.set_color(&spec).unwrap();
-                write!(stdout, "{}", segment).unwrap();
-                stdout.set_color(&ColorSpec::new()).unwrap();
+
+        // build line segments
+        let line_segments: Vec<_> = self.lines[start..end]
+            .iter()
+            .map(|line| {
+                let mut segments = vec![];
+                if self.query.is_empty() {
+                    segments.push(line.as_str().stylize());
+                } else {
+                    let mut curr_idx = 0;
+                    for (next_idx, substr) in line.match_indices(&self.query) {
+                        let normal = &line[curr_idx..next_idx];
+                        segments.push(normal.stylize());
+                        segments.push(substr.with(Color::Red));
+                        curr_idx = next_idx + substr.len();
+                    }
+                    segments.push((&line[curr_idx..]).stylize());
+                };
+                segments
+            })
+            .collect();
+
+        // enqueue commands
+        for (no, segments) in line_segments.iter().enumerate() {
+            queue!(stdout, MoveTo(0, no as u16), Clear(ClearType::CurrentLine)).unwrap();
+            for segment in segments {
+                stdout.queue(PrintStyledContent(*segment)).unwrap();
             }
-            writeln!(stdout).unwrap();
         }
 
         let message = self
@@ -297,14 +300,22 @@ impl Screen {
             .as_ref()
             .cloned()
             .unwrap_or_else(|| self.query.clone());
-        stdout
-            .queue(MoveTo(0, self.contents_height() as u16))
-            .unwrap();
-        stdout.queue(Clear(ClearType::CurrentLine)).unwrap();
-        print!("{}{}", if self.query_mode { '/' } else { ':' }, message);
+        queue!(
+            stdout,
+            MoveTo(0, self.contents_height() as u16),
+            Clear(ClearType::CurrentLine),
+            Print(format_args!(
+                "{}{}",
+                if self.query_mode { '/' } else { ':' },
+                message
+            ))
+        )
+        .unwrap();
+
         *self.message.borrow_mut() = None;
-        self.needs_update.set(false);
         stdout.flush().unwrap();
+
+        self.needs_update.set(false);
     }
 
     fn contents_height(&self) -> usize {
